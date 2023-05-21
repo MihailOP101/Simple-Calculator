58e8af825d640652ea357a99b42ac0773ef2efc4
MihailOP101 <mishoisapro@gmail.com>
2023-05-21 02:28:44 +0000

    Adds comments

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 //This is the main class where all the different calculations will be happening
 public class Main
 {
+  //The main method that gets all the numbers and see what needs to be done.
   public static void main(String[] args)
   {
     System.out.println("Hello new project!");
